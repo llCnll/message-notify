44cedf92caf6bf6007397b68452a3895d7dc42cb
llCnll <972581592@qq.com>
2023-02-10 01:02:15 +0800

    feat(init): init.

---
+++ src/main/java/cn/chennan/messagenotify/MessageNotifyApplication.java
@@ -0,0 +1,13 @@
+package cn.chennan.messagenotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageNotifyApplication.class, args);
+	}
+
+}
